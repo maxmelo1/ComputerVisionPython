df98be4b1e878aadd8fc6fc1a82070e1912d4af4
maxmelo1 <max.mjm.melo@gmail.com>
2023-06-12 20:53:32 -0400

    refactor: add support for both training and eval
    Arg: mode [train, eval]. Train fits for 10 epochs. Eval shows val acc.

--- Localizing anomalies in images/README.md
+++ Localizing anomalies in images/README.md
@@ -1,2 +1,9 @@
-#Localizing anomalies in images
+# Localizing anomalies in images
 
+This project ilustrates the Global Average Pooling (GAP) capabilities of replacing fully connected blocks and also enables the Class Activation Map (CAM) to ilustrate localization maps from the classification output.
+
+A simple VGG-16 is used as a CNN classification model. The model output classifier is replaced by a version in which GAP is applied. The fully conected layer is held, but can be removed without major modifications. 
+
+## Source
+
+This project is an adaptation to Pytorch from [Dr. Sreenivas Bhattiprolu (Sreeni) GAP example](https://github.com/bnsreenu/python_for_microscopists/blob/master/261_global_average_pooling/261_global_average_pooling.py) and his [Youtube explanation video](https://www.youtube.com/watch?v=gNRVTCf6lvY). 
--- Localizing anomalies in images/train.py
+++ Localizing anomalies in images/train.py
@@ -19,6 +19,7 @@ from tqdm import tqdm
 from model import Model, GAP
 from customdatasetclassification import CustomDatasetClassification
 from params import *
+import argparse
 
 
 dataset = np.array([])
@@ -28,8 +29,11 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 # torch.autograd.set_detect_anomaly(True)
 
+parser = argparse.ArgumentParser('GAP example')
 
+parser.add_argument('--mode', default='train', choices=['train', 'eval'])
 
+args = parser.parse_args()
 
 train_transform = A.Compose(
     [
@@ -99,32 +103,40 @@ val_loader = DataLoader(
     shuffle=False
 )
 
+def evaluate(loader, model, criterion):
+    model.eval()
+    sum_loss = 0.0
+    sum_acc  = 0.0
+    with torch.no_grad():
+        for i, (im_name, x, y) in enumerate(loader):
+            x = x.to(DEVICE).float()
+            y = y.to(DEVICE).long()#.unsqueeze(1).float()
 
-model = Model(n_classes=2)
-print(model)
+            pred = model(x)
+            loss = criterion(pred, y)
+            sum_loss += loss
 
-_,x,y = next(iter(train_loader))
-model.to(DEVICE)
-x = x.to(DEVICE)
+            # y_pred = torch.zeros(pred.size(), dtype=torch.float32).to(DEVICE)
+            # y_pred[pred>0.5] = 1
+            y_pred = torch.argmax(torch.softmax(pred, dim=1), dim=1).long()
 
-print( summary(model, (3, 224, 224)) )
+            acc = torch.sum(y_pred == y).cpu().detach().item() / BS
+            sum_acc += acc
+
+    sum_loss = sum_loss / (i+1)
+    sum_acc  = sum_acc  / (i+1)
+    return sum_loss, sum_acc
 
 
-ct = 0
-for child in model.features.children():
-    if ct < 17:
-        # print(child._get_name())
-        for param in child.parameters():
-            param.requires_grad = False
-    ct += 1
+
+
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=0.9)
-scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.95)
+
 scaler = torch.cuda.amp.GradScaler()
 
-model.to(DEVICE)
-model.train()
+model_path = 'best_model.pth'
+
 train_loss = 0.0
 best_loss = 10000.
 train_acc = 0.0
@@ -132,91 +144,112 @@ train_acc = 0.0
 train_log = {'loss' : [], 'acc': []}
 val_log = {'loss' : [], 'acc': []}
 
-for epoch in range(NUM_EPOCHS):
-    loop = tqdm(train_loader, unit="batch")
-    for i, (im_name,x,y) in enumerate(loop):
-        loop.set_description(f'Epoch: {epoch}, batch {i}')
-        
-        x = x.to(DEVICE).float()
-        y = y.to(DEVICE).long()#.unsqueeze(0)#.float()
 
-        # with torch.cuda.amp.autocast():
-        pred = model(x)
-        # pred = torch.argmax(pred, dim=1)
-        
-        loss = criterion(pred, y)
-        train_loss += loss
-        # y_pred = torch.zeros(pred.size(), dtype=torch.float32).to(DEVICE)
-        # y_pred[pred>0.5] = 1.0
-        y_pred = torch.argmax(torch.softmax(pred, dim=1), dim=1).long()
-        acc = torch.sum(y_pred == y).cpu().detach().item() / BS
-        train_acc += acc
-        
-        
-        optimizer.zero_grad()
-        scaler.scale(loss).backward()
-        scaler.step(optimizer)
-        scaler.update()
+if args.mode == 'train':
+    model = Model(n_classes=2)
+    print(model)
 
-        # loop.set_postfix(loss=loss.item(), acc=acc)
-        loop.set_postfix(loss= train_loss.item() / (i+1), acc= train_acc / (i+1))
+    ct = 0
+    for child in model.features.children():
+        if ct < 17:
+            # print(child._get_name())
+            for param in child.parameters():
+                param.requires_grad = False
+        ct += 1
 
-    train_loss = train_loss / (i+1)
-    train_acc  = train_acc  / (i+1)
-    print(f'On epoch end, train loss: {train_loss}, acc: {train_acc}')
-    train_log['loss'].append(train_loss.cpu().detach())
-    train_log['acc'].append(train_acc)
+    optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=0.9)
+    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.95)
 
-    print('evaluating model:')
-    model.eval()
-    val_loss = 0.0
-    val_acc  = 0.0
-    with torch.no_grad():
-        for i, (im_name, x, y) in enumerate(val_loader):
+    _,x,y = next(iter(train_loader))
+    model.to(DEVICE)
+    x = x.to(DEVICE)
+
+    print( summary(model, (3, 224, 224)) )
+
+    model.to(DEVICE)
+    model.train()
+    for epoch in range(NUM_EPOCHS):
+        loop = tqdm(train_loader, unit="batch")
+        for i, (im_name,x,y) in enumerate(loop):
+            loop.set_description(f'Epoch: {epoch}, batch {i}')
+
             x = x.to(DEVICE).float()
-            y = y.to(DEVICE).long()#.unsqueeze(1).float()
+            y = y.to(DEVICE).long()#.unsqueeze(0)#.float()
 
+            # with torch.cuda.amp.autocast():
             pred = model(x)
+            # pred = torch.argmax(pred, dim=1)
+
             loss = criterion(pred, y)
-            val_loss += loss
-            
+            train_loss += loss
             # y_pred = torch.zeros(pred.size(), dtype=torch.float32).to(DEVICE)
-            # y_pred[pred>0.5] = 1
+            # y_pred[pred>0.5] = 1.0
             y_pred = torch.argmax(torch.softmax(pred, dim=1), dim=1).long()
-
             acc = torch.sum(y_pred == y).cpu().detach().item() / BS
-            val_acc += acc
+            train_acc += acc
 
-    val_loss = val_loss / (i+1)
-    val_acc  = val_acc  / (i+1)
-    print(f'Validation loss: {val_loss}, val acc: {val_acc}')
-    val_log['loss'].append(val_loss.cpu().detach())
-    val_log['acc'].append(val_acc)
 
-    if val_log['loss'][-1] < best_loss:
-        best_loss = val_log['loss'][-1]
-        print(f'New Best loss found: {best_loss}. Saving model')
-        model_path = 'best_model.pth'
-        torch.save(model.state_dict(), model_path)
+            optimizer.zero_grad()
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+
+            # loop.set_postfix(loss=loss.item(), acc=acc)
+            loop.set_postfix(loss= train_loss.item() / (i+1), acc= train_acc / (i+1))
+
+        train_loss = train_loss / (i+1)
+        train_acc  = train_acc  / (i+1)
+        print(f'On epoch end, train loss: {train_loss}, acc: {train_acc}')
+        train_log['loss'].append(train_loss.cpu().detach())
+        train_log['acc'].append(train_acc)
+
+        print('evaluating model:')
+        model.eval()
+        
+
+        val_loss, val_acc = evaluate(val_loader, model=model, criterion=criterion)
+        
+        print(f'Validation loss: {val_loss}, val acc: {val_acc}')
+        val_log['loss'].append(val_loss.cpu().detach())
+        val_log['acc'].append(val_acc)
+
+        if val_log['loss'][-1] < best_loss:
+            best_loss = val_log['loss'][-1]
+            print(f'New Best loss found: {best_loss}. Saving model')
+            
+            torch.save(model.state_dict(), model_path)
+
+        scheduler.step()
+
+
+    #plotar os gráficos
+    epochs = range(1, NUM_EPOCHS+1)
+    plt.plot(epochs, train_log['loss'], 'y', label='Training loss')
+    plt.plot(epochs, val_log['loss'], 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig('loss.png')
+    plt.show()
+
+    plt.plot(epochs, train_log['acc'], 'y', label='Training acc')
+    plt.plot(epochs, val_log['acc'], 'r', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.savefig('acc.png')
+    plt.show()
+
+elif args.mode == 'eval':
+    model = Model(n_classes=2)
+    model.load_state_dict(torch.load(model_path))
+    model.to(DEVICE)
     
-    scheduler.step()
+    print(model)
+    print('Model loaded from disk!')
 
-#plotar os gráficos
-epochs = range(1, NUM_EPOCHS+1)
-plt.plot(epochs, train_log['loss'], 'y', label='Training loss')
-plt.plot(epochs, val_log['loss'], 'r', label='Validation loss')
-plt.title('Training and validation loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-plt.savefig('loss.png')
-plt.show()
+    test_loss, test_acc = evaluate(loader=val_loader, model=model, criterion=criterion)
 
-plt.plot(epochs, train_log['acc'], 'y', label='Training acc')
-plt.plot(epochs, val_log['acc'], 'r', label='Validation acc')
-plt.title('Training and validation accuracy')
-plt.xlabel('Epochs')
-plt.ylabel('Accuracy')
-plt.legend()
-plt.savefig('acc.png')
-plt.show()
+    print(f'Val loss: {test_loss}, val acc: {test_acc}')
